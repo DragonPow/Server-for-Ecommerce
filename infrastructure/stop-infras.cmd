0d455d81b553df078df2d02cf309018159ded43c
Hung Nguyen <hungntsol77@gmail.com>
2022-10-11 20:10:51 +0700

    add docker

---
+++ infrastructure/stop-infras.cmd
@@ -0,0 +1,13 @@
+title 'Stop ktpm-server-infrastructure'
+
+:: move to working directory
+cd ../
+
+:: compose up
+docker-compose -f docker-compose.dev.yml -p ktpm-server-infra down
+
+:: post-action
+@echo off
+@echo ktmp-server-infra compose up, auto close in 5s
+
+timeout 5
